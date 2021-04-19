f43dc3fc6542ffa12b2ea4443054023e841209aa
Nadya <nadejda.kozhukh@gmail.com>
2021-04-19 13:24:58 +0300

    js

---
+++ js/js-task_1/kata.js
@@ -0,0 +1,13 @@
+function greet(name){
+
+  return ;
+}
+
+const areaOrPerimeter = function(l , w) {
+  return ;
+};
+
+function dutyFree(normPrice, discount, hol){
+  return Math.floor(/* формула */)
+}
+
